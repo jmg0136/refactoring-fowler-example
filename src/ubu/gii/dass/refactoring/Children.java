a79f569c506b58eeefb52717cf12a7fa000f5458
jmg0136 <jmg0136@alu.ubu.es>
2021-05-27 18:15:29 +0200

    Add method getTypeCode()

---
+++ src/ubu/gii/dass/refactoring/Children.java
@@ -0,0 +1,10 @@
+package ubu.gii.dass.refactoring;
+
+public class Children extends MovieType {
+
+	@Override
+	public int getTypeCode() {
+		return MovieType.CHILDRENS;
+	}
+
+}
--- src/ubu/gii/dass/refactoring/MovieType.java
+++ src/ubu/gii/dass/refactoring/MovieType.java
@@ -5,5 +5,7 @@ public abstract class MovieType {
 	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
+	
+ 	public abstract int getTypeCode();
 
 }

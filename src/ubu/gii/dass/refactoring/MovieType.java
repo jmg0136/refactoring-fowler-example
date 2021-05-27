61bc448ad463a24257719862b3a80e677ddb6032
jmg0136 <jmg0136@alu.ubu.es>
2021-05-27 18:02:35 +0200

    Move constants from Movie to MovieType

---
+++ src/ubu/gii/dass/refactoring/MovieType.java
@@ -0,0 +1,9 @@
+package ubu.gii.dass.refactoring;
+
+public abstract class MovieType {
+
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+
+}
